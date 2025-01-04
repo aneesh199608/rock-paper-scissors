d5a13eb1c6989fb636079f4ac275a861ca9751cd
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-01-04 07:44:56 +0530

    chore: add pseudocode for game logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+//Function "getComputerChoice" returns random string values "rock" or "paper" or "scissors"
+//Function "getHumanChoice" for user to input his choice via prompt
+//Function "playRound" to play a single round
+//  -Compare computer and user choices
+//  -Determine the winner
+//  -Increment the winner's score
+//  -Display winner
+//Function "playGame" for user to play 5 rounds and keep track
+//Declare final winner
