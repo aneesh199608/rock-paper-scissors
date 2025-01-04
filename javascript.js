2a40bfd3a9b9afe0ad98dc1d6795b2a9151625e9
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-01-04 08:17:15 +0530

    feat: implement logic for getting user choice

--- javascript.js
+++ javascript.js
@@ -3,16 +3,20 @@
 function getComputerChoice() {
     let randomNumber = Math.random()*100;
     if (randomNumber>=0 && randomNumber<33) {
-        return computerChoice = "Rock";
+        return "Rock";
     } else if (randomNumber>=33 && randomNumber<66) {
-        return computerChoice = "Paper";
+        return "Paper";
     } else if (randomNumber>66) {
-        return computerChoice = "Scissors";
+        return "Scissors";
     }
 }
-console.log(getComputerChoice());
+
 
 //Function "getHumanChoice" for user to input his choice via prompt
+
+function getHumanChoice() {
+    return prompt('Welcome to our game! Type your choice of "Rock Paper Scissors"');
+}
 //Function "playRound" to play a single round
 //  -Compare computer and user choices
 //  -Determine the winner
@@ -20,3 +24,6 @@ console.log(getComputerChoice());
 //  -Display winner
 //Function "playGame" for user to play 5 rounds and keep track
 //Declare final winner
+
+console.log("Human choice: " + getHumanChoice());
+console.log("Computer choice: " + getComputerChoice());
